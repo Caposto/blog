3246c27c586d059f2322b10fdeed1837068f1354
ChristianA <christiand.apostol@outlool.com>
2022-05-14 17:09:43 -0400

    added blog post database

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,46 +1,58 @@
+from asyncio import tasks
 import os
 from pickle import STACK_GLOBAL
 
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # To run the application
 # set FLASK_APP=flaskr
 # set FLASK_ENV=development
 # flask run
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True, static_url_path='/static')
-    app.config.from_mapping(
+app = Flask(__name__, instance_relative_config=True, static_url_path='/static')
+app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-        STATIC_FOLDER = os.path.join('static', 'blog_photo'),
-    )
+        SQLALCHEMY_TRACK_MODIFICATIONS = False,
+)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+# Setup Database with SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+db.create_all()
+# a simple page that says hello
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+# Routes user to the blog page
+# is url_for() method better?
+@app.route('/blog', methods=['POST', 'GET'])
+def blog_index():
+    if request.method == 'POST':
+        post_body = request.form['body']
+        new_post = Post(body=post_body)
+
+        try:
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect('/blog')
+        except:
+            return "There was an error uploading your post"
     else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+        posts = Post.query.order_by(Post.date_created).all()
+        return render_template("blog.html", posts=posts)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# Class representing a blog post
+class Post(db.Model):
+    title = db.Column(db.String(50), primary_key=True)
+    body = db.Column(db.String(1000))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    # a simple page that says hello
-    @app.route('/')
-    def index():
-        return render_template("index.html")
+    def __repr__(self):
+        return '<Task %r' % self.id
 
-    # Routes user to the blog page
-    # is url_for() method better?
-    @app.route('/blog')
-    def blog_index():
-        return render_template("blog.html")
-
-    return app
-
+if __name__ == "__main__":
+    app.run(debug=True)
