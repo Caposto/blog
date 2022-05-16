4e1fc2e3d5454fdd301d309afe989d81de033144
ChristianA <christiand.apostol@outlool.com>
2022-05-16 16:22:01 -0400

    operational error stuck

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,29 +5,38 @@ from datetime import datetime
 # Import configuration modules
 from instance.config import DevelopmentConfig
 
-# To run the application
-# set FLASK_APP=flaskr
-# set FLASK_ENV=development
-# flask run
-
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(DevelopmentConfig)
 
-# Setup Database with SQLAlchemy
+# Initialize Database
 db = SQLAlchemy(app)
-db.create_all()
-# a simple page that says hello
+
+# Welcome Page
 @app.route('/')
 def index():
     return render_template("index.html")
+
+# Class representing a blog post
+class BlogPost(db.Model):
+    title = db.Column(db.String(50), primary_key=True)
+    body = db.Column(db.String(1000), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+    def __repr__(self):
+        return '<Blog Post %r>' % self.title
 
 # Routes user to the blog page
 # is url_for() method better?
 @app.route('/blog', methods=['POST', 'GET'])
 def blog_index():
     if request.method == 'POST':
+        post_title = request.form['title']
         post_body = request.form['body']
-        new_post = blog_post(body=post_body)
+        new_post = BlogPost(title=post_title, body=post_body)
 
         try:
             db.session.add(new_post)
@@ -36,17 +45,9 @@ def blog_index():
         except:
             return "There was an error uploading your post"
     else:
-        blog_posts = blog_post.query.order_by(blog_post.date_created).all()
+        blog_posts = BlogPost.query.order_by(BlogPost.date_created).all()
         return render_template("blog.html", blog_posts=blog_posts)
 
-# Class representing a blog post
-class blog_post(db.Model):
-    title = db.Column(db.String(50), primary_key=True)
-    body = db.Column(db.String(1000))
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r' % self.id
-
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
