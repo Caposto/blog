a32ab935394745854f56607e45c16b70401121e1
ChristianA <christiand.apostol@outlool.com>
2022-05-22 14:54:59 -0400

    fixed operational error

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,23 +11,21 @@ app.config.from_object(DevelopmentConfig)
 # Initialize Database
 db = SQLAlchemy(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 # Welcome Page
 @app.route('/')
 def index():
     return render_template("index.html")
 
 # Class representing a blog post
-class BlogPost(db.Model):
-    title = db.Column(db.String(50), primary_key=True)
-    body = db.Column(db.String(1000), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
-
-    def __repr__(self):
-        return '<Blog Post %r>' % self.title
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
 
 # Routes user to the blog page
 # is url_for() method better?
@@ -36,7 +34,7 @@ def blog_index():
     if request.method == 'POST':
         post_title = request.form['title']
         post_body = request.form['body']
-        new_post = BlogPost(title=post_title, body=post_body)
+        new_post = Blogpost(title=post_title, body=post_body)
 
         try:
             db.session.add(new_post)
@@ -45,9 +43,8 @@ def blog_index():
         except:
             return "There was an error uploading your post"
     else:
-        blog_posts = BlogPost.query.order_by(BlogPost.date_created).all()
-        return render_template("blog.html", blog_posts=blog_posts)
+        posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+        return render_template("blog.html", posts=posts)
 
 if __name__ == "__main__":
-    db.create_all()
     app.run(debug=True)
