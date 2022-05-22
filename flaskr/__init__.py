1278b3671f9a5459f7222da58bfdedb05e82416a
ChristianA <christiand.apostol@outlool.com>
2022-05-22 15:12:56 -0400

    created add blog functionality

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -24,11 +24,16 @@ def index():
 class Blogpost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
-    date_posted = db.Column(db.DateTime)
     content = db.Column(db.Text)
+    date_posted = db.Column(db.DateTime)
+
+    def __init__(self, title, content, date_posted=datetime.now()):
+        self.title = title
+        self.content = content
+        self.date_posted = date_posted
+    
 
 # Routes user to the blog page
-# is url_for() method better?
 @app.route('/blog', methods=['POST', 'GET'])
 def blog_index():
     if request.method == 'POST':
@@ -45,6 +50,24 @@ def blog_index():
     else:
         posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
         return render_template("blog.html", posts=posts)
+
+# Add a new blog post using information from html form
+@app.route('/add_post', methods=["POST"])
+def add_post():
+    title = request.form['title']
+    content = request.form['body']
+
+    new_post = Blogpost(title, content)
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(url_for('blog_index'))
+
+@app.roue('delete_post', methods=["POST"])
+def delete_post():
+    
+    return redirect(url_for('blog_index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
